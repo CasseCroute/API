ae88843717a4150f2ca37bc004e32eb1890d0035
Alassane Coly-Arogundade <bonjour@alassanecoly.fr>
2018-10-28 22:33:53 +0100

    tests: Write HTTP tests

--- src/interfaces/http/modules/store/__tests__/store.meals.http.spec.ts
+++ src/interfaces/http/modules/store/__tests__/store.meals.http.spec.ts
@@ -452,6 +452,392 @@ describe('Store Meals HTTP Requests', () => {
 		});
 	});
 
+	describe('PATCH stores/me/meals/:uuid', () => {
+		it('should return a HTTP 204 status code when successful', () => {
+			return request(app.getHttpServer())
+				.patch(`/stores/me/meals/${mocks.mealRepository.data[0].uuid}`)
+				.set('Authorization', `Bearer ${mocks.token}`)
+				.send({reference: 'MENU', name: 'Menu Burger', price: '12'})
+				.expect(204);
+		});
+
+		it('should return a HTTP 204 status code when meal has 1 subsection', () => {
+			return request(app.getHttpServer())
+				.patch(`/stores/me/meals/${mocks.mealRepository.data[0].uuid}`)
+				.set('Authorization', `Bearer ${mocks.token}`)
+				.send({
+					reference: 'MENU',
+					name: 'Menu Burger',
+					price: '12',
+					subsections: [
+						{
+							name: 'Dessert',
+							isRequired: true,
+							minSelectionsPermitted: 1,
+							maxSelectionsPermitted: 1
+						}
+					]
+				})
+				.expect(204);
+		});
+
+		it('should return a HTTP 204 status code when Meal has 1 Subsection with 1 option Product', () => {
+			return request(app.getHttpServer())
+				.patch(`/stores/me/meals/${mocks.mealRepository.data[0].uuid}`)
+				.set('Authorization', `Bearer ${mocks.token}`)
+				.send({
+					reference: 'MENU',
+					name: 'Menu Burger',
+					price: '12',
+					subsections: [
+						{
+							name: 'Dessert',
+							isRequired: true,
+							minSelectionsPermitted: 1,
+							maxSelectionsPermitted: 1,
+							options: {
+								products: [
+									{
+										optionProductUuid: mocks.productRepository.data[1].uuid
+									}
+								]
+							}
+						}
+					]
+				})
+				.expect(204);
+		});
+
+		it('should return a HTTP 204 status code when Meal has 1 Subsection with multiple options Products', () => {
+			return request(app.getHttpServer())
+				.patch(`/stores/me/meals/${mocks.mealRepository.data[0].uuid}`)
+				.set('Authorization', `Bearer ${mocks.token}`)
+				.send({
+					reference: 'MENU',
+					name: 'Menu Burger',
+					price: '12',
+					subsections: [
+						{
+							name: 'Dessert',
+							isRequired: true,
+							minSelectionsPermitted: 1,
+							maxSelectionsPermitted: 1,
+							options: {
+								products: [
+									{
+										optionProductUuid: mocks.productRepository.data[1].uuid
+									},
+									{
+										optionProductUuid: mocks.productRepository.data[0].uuid
+									}
+								]
+							}
+						}
+					]
+				})
+				.expect(204);
+		});
+
+		it('should return a HTTP 204 status code when Meal has 1 Subsection with 1 option Ingredient', () => {
+			return request(app.getHttpServer())
+				.patch(`/stores/me/meals/${mocks.mealRepository.data[0].uuid}`)
+				.set('Authorization', `Bearer ${mocks.token}`)
+				.send({
+					reference: 'MENU',
+					name: 'Menu Burger',
+					price: '12',
+					subsections: [
+						{
+							name: 'Dessert',
+							isRequired: true,
+							minSelectionsPermitted: 1,
+							maxSelectionsPermitted: 1,
+							options: {
+								ingredients: [
+									{
+										optionIngredientUuid: mocks.ingredientRepository.data[0].uuid
+									}
+								]
+							}
+						}
+					]
+				})
+				.expect(204);
+		});
+
+		it('should return a HTTP 204 status code when Meal has 1 Subsection with multiple option Ingredients', () => {
+			return request(app.getHttpServer())
+				.patch(`/stores/me/meals/${mocks.mealRepository.data[0].uuid}`)
+				.set('Authorization', `Bearer ${mocks.token}`)
+				.send({
+					reference: 'MENU',
+					name: 'Menu Burger',
+					price: '12',
+					subsections: [
+						{
+							name: 'Dessert',
+							isRequired: true,
+							minSelectionsPermitted: 1,
+							maxSelectionsPermitted: 1,
+							options: {
+								ingredients: [
+									{
+										optionIngredientUuid: mocks.ingredientRepository.data[0].uuid
+									},
+									{
+										optionIngredientUuid: mocks.ingredientRepository.data[1].uuid
+									}
+								]
+							}
+						}
+					]
+				})
+				.expect(204);
+		});
+
+		it('should return a HTTP 204 status code when Meal has 1 Subsection with 1 option Product and 1 option Ingredient', () => {
+			return request(app.getHttpServer())
+				.patch(`/stores/me/meals/${mocks.mealRepository.data[0].uuid}`)
+				.set('Authorization', `Bearer ${mocks.token}`)
+				.send({
+					reference: 'MENU',
+					name: 'Menu Burger',
+					price: '12',
+					subsections: [
+						{
+							name: 'Dessert',
+							isRequired: true,
+							minSelectionsPermitted: 1,
+							maxSelectionsPermitted: 1,
+							options: {
+								products: [
+									{
+										optionProductUuid: mocks.productRepository.data[1].uuid
+									}
+								],
+								ingredients: [
+									{
+										optionIngredientUuid: mocks.ingredientRepository.data[0].uuid
+									}
+								]
+							}
+						}
+					]
+				})
+				.expect(204);
+		});
+
+		it('should return a HTTP 204 status code when Meal has 1 Subsection with multiple option Product and Ingredient', () => {
+			return request(app.getHttpServer())
+				.patch(`/stores/me/meals/${mocks.mealRepository.data[0].uuid}`)
+				.set('Authorization', `Bearer ${mocks.token}`)
+				.send({
+					reference: 'MENU',
+					name: 'Menu Burger',
+					price: '12',
+					subsections: [
+						{
+							name: 'Dessert',
+							isRequired: true,
+							minSelectionsPermitted: 1,
+							maxSelectionsPermitted: 1,
+							options: {
+								products: [
+									{
+										optionProductUuid: mocks.productRepository.data[1].uuid
+									},
+									{
+										optionProductUuid: mocks.productRepository.data[0].uuid
+									}
+								],
+								ingredients: [
+									{
+										optionIngredientUuid: mocks.ingredientRepository.data[0].uuid
+									},
+									{
+										optionIngredientUuid: mocks.ingredientRepository.data[1].uuid
+									}
+								]
+							}
+						}
+					]
+				})
+				.expect(204);
+		});
+
+		it('should return a HTTP 204 status code when meal has 1 subsection with incorrect data', () => {
+			return request(app.getHttpServer())
+				.patch(`/stores/me/meals/${mocks.mealRepository.data[0].uuid}`)
+				.set('Authorization', `Bearer ${mocks.token}`)
+				.send({
+					reference: 'MENU',
+					name: 'Menu Burger',
+					price: '12',
+					subsections: [
+						{
+							what: 'Dessert',
+							isRequired: true,
+							minSelectionsPermitted: 1,
+							maxSelectionsPermitted: 1
+						}
+					]
+				})
+				.expect(400);
+		});
+
+		it('should return a HTTP 204 status code when meal has multiple subsections', () => {
+			return request(app.getHttpServer())
+				.patch(`/stores/me/meals/${mocks.mealRepository.data[0].uuid}`)
+				.set('Authorization', `Bearer ${mocks.token}`)
+				.send({
+					reference: 'MENU',
+					name: 'Menu Burger',
+					price: '12',
+					subsections: [
+						{
+							name: 'Dessert',
+							isRequired: true,
+							minSelectionsPermitted: 1,
+							maxSelectionsPermitted: 1
+						},
+						{
+							name: 'Extra',
+							isRequired: false,
+							minSelectionsPermitted: 1,
+							maxSelectionsPermitted: 2
+						}
+					]
+				})
+				.expect(204);
+		});
+
+		it('should return a HTTP 204 status code when meal has multiple subsections with multiple option Products and Ingredients', () => {
+			return request(app.getHttpServer())
+				.patch(`/stores/me/meals/${mocks.mealRepository.data[0].uuid}`)
+				.set('Authorization', `Bearer ${mocks.token}`)
+				.send({
+					reference: 'MENU',
+					name: 'Menu Burger',
+					price: '12',
+					subsections: [
+						{
+							name: 'Dessert',
+							isRequired: true,
+							minSelectionsPermitted: 1,
+							maxSelectionsPermitted: 1,
+							options: {
+								products: [
+									{
+										optionProductUuid: mocks.productRepository.data[1].uuid
+									},
+									{
+										optionProductUuid: mocks.productRepository.data[0].uuid
+									}
+								],
+								ingredients: [
+									{
+										optionIngredientUuid: mocks.ingredientRepository.data[0].uuid
+									},
+									{
+										optionIngredientUuid: mocks.ingredientRepository.data[1].uuid
+									}
+								]
+							}
+						},
+						{
+							name: 'Extra',
+							isRequired: false,
+							minSelectionsPermitted: 1,
+							maxSelectionsPermitted: 2,
+							options: {
+								products: [
+									{
+										optionProductUuid: mocks.productRepository.data[1].uuid
+									},
+									{
+										optionProductUuid: mocks.productRepository.data[0].uuid
+									}
+								],
+								ingredients: [
+									{
+										optionIngredientUuid: mocks.ingredientRepository.data[0].uuid
+									},
+									{
+										optionIngredientUuid: mocks.ingredientRepository.data[1].uuid
+									}
+								]
+							}
+						}
+					]
+				})
+				.expect(204);
+		});
+
+		it('should return a HTTP 204 status code when meal has multiple subsections with incorrect data', () => {
+			return request(app.getHttpServer())
+				.patch(`/stores/me/meals/${mocks.mealRepository.data[0].uuid}`)
+				.set('Authorization', `Bearer ${mocks.token}`)
+				.send({
+					reference: 'MENU',
+					name: 'Menu Burger',
+					price: '12',
+					subsections: [
+						{
+							name: 'Dessert',
+							isRequired: true,
+							minSelectionsPermitted: 1,
+							maxSelectionsPermitted: 1
+						},
+						{
+							uh: 'Extra',
+							isRequired: false,
+							minSelectionsPermitted: 1,
+							maxSelectionsPermitted: 2
+						}
+					]
+				})
+				.expect(400);
+		});
+
+		it('should return an empty body when successful', () => {
+			return request(app.getHttpServer())
+				.patch(`/stores/me/meals/${mocks.mealRepository.data[0].uuid}`)
+				.set('Authorization', `Bearer ${mocks.token}`)
+				.send({reference: 'MENU', name: 'Menu Burger', price: '12'})
+				.expect((res: any) => {
+					res.body = '';
+				});
+		});
+
+		it('should return a HTTP 401 status code when no JWT is provided in Authorization header', () => {
+			return request(app.getHttpServer())
+				.patch(`/stores/me/meals/${mocks.mealRepository.data[0].uuid}`)
+				.send({reference: 'MENU', name: 'Menu Burger', price: '12', productUuid: mocks.productRepository.data[0].uuid})
+				.expect(401);
+		});
+
+		it('should return a HTTP 400 status code when Meal reference length is gretear than 16', () => {
+			return request(app.getHttpServer())
+				.patch(`/stores/me/meals/${mocks.mealRepository.data[0].uuid}`)
+				.set('Authorization', `Bearer ${mocks.token}`)
+				.send({
+					reference: 'MENUMENUMENUMENUMENU',
+					name: 'Menu Burger',
+					price: '12',
+					productUuid: mocks.productRepository.data[0].uuid
+				})
+				.expect(400);
+		});
+
+		it('should return a HTTP 400 status code when incorrect data is sent', () => {
+			return request(app.getHttpServer())
+				.patch(`/stores/me/meals/${mocks.mealRepository.data[0].uuid}`)
+				.set('Authorization', `Bearer ${mocks.token}`)
+				.send({ref: 'MENU', name: 'Menu Burger', price: '12', productUuid: mocks.productRepository.data[0].uuid})
+				.expect(400);
+		});
+	});
+
 	afterAll(async () => {
 		await app.close();
 	});
