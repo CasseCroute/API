2b6ba06200134dc3c1bc0893025df43453ef4b73
Alassane Coly-Arogundade <bonjour@alassanecoly.fr>
2018-11-05 23:45:13 +0100

    fix + refactor: Product & Meal creation (#148)
    * fix + refactor: Product & Meal creation
    
    * enhance: Remove Unique Meal reference column
    
    * fix(ci): Fix TSLint issue

--- src/application/commands/product/handlers/update-product.handler.ts
+++ src/application/commands/product/handlers/update-product.handler.ts
@@ -1,12 +1,11 @@
 import {ICommandHandler, CommandHandler} from '@nestjs/cqrs';
-import {getCustomRepository, Repository} from 'typeorm';
+import {getCustomRepository} from 'typeorm';
 import {BadRequestException} from '@nestjs/common';
 import {StoreRepository} from '@letseat/infrastructure/repository/store.repository';
 import {UpdateProductCommand} from '@letseat/application/commands/product';
 import {ProductRepository} from '@letseat/infrastructure/repository/product.repository';
 import {Product} from '@letseat/domains/product/product.entity';
 import {ProductIngredientRepository} from '@letseat/infrastructure/repository/product-ingredient.repository';
-import {ProductIngredient} from '@letseat/domains/product-ingredient/product-ingredient.entity';
 
 @CommandHandler(UpdateProductCommand)
 export class UpdateProductHandler implements ICommandHandler<UpdateProductCommand> {
@@ -21,7 +20,7 @@ export class UpdateProductHandler implements ICommandHandler<UpdateProductComman
 
 			if (product.ingredients && product.ingredients.length > 0) {
 				const productFound = await productRepository.findStoreProductByUuid(command.storeUuid, command.productUuid);
-				await productIngredientRepository.updateStoreProductIngredients(command.storeUuid, productFound as Product, product, new Repository<ProductIngredient>());
+				await productIngredientRepository.updateStoreProductIngredients(command.storeUuid, productFound as Product, product);
 			}
 			resolve();
 		} catch (err) {
--- src/infrastructure/repository/product-ingredient.repository.ts
+++ src/infrastructure/repository/product-ingredient.repository.ts
@@ -1,76 +1,63 @@
-/* tslint:disable */
+/* tslint:disable:no-duplicate-string */
 import {
 	EntityRepository,
-	Repository,
-	Transaction,
-	TransactionManager, createQueryBuilder, getManager, getCustomRepository,
+	Repository, createQueryBuilder, getManager, getCustomRepository,
 } from 'typeorm';
 import {ResourceRepository} from '@letseat/infrastructure/repository/resource.repository';
 import {Ingredient} from '@letseat/domains/ingredient/ingredient.entity';
 import {Product} from '@letseat/domains/product/product.entity';
 import {ProductIngredient} from '@letseat/domains/product-ingredient/product-ingredient.entity';
 import {CreateProductDto} from '@letseat/domains/product/dtos';
-import {UpdateProductDto} from "@letseat/domains/product/dtos/update-product.dto";
-import {IngredientRepository} from "@letseat/infrastructure/repository/ingredient.repository";
+import {UpdateProductDto} from '@letseat/domains/product/dtos/update-product.dto';
+import {IngredientRepository} from '@letseat/infrastructure/repository/ingredient.repository';
 
 @EntityRepository(ProductIngredient)
 export class ProductIngredientRepository extends Repository<ProductIngredient> implements ResourceRepository {
-	@Transaction()
-
 	public async findOneByUuid(productIngredientUuid: string) {
 		return this.findOne({where: {uuid: productIngredientUuid}});
 	}
 
-	@Transaction()
 	public async saveStoreProductIngredients(
 		storeUuid: string,
-		product: CreateProductDto & Product,
-		@TransactionManager() productIngredientRepository: Repository<ProductIngredient>) {
+		product: CreateProductDto & Product) {
 		const savedProduct = await getManager()
 			.findOne(Product, {where: {id: product.id}}) as Product;
-		product.ingredients.forEach(async (productIng) => {
+		product.ingredients.forEach(async productIng => {
 			const productIngredient = new ProductIngredient();
 			const ingredient = await createQueryBuilder(Ingredient, 'ingredient')
 				.leftJoinAndSelect('ingredient.store', 'store')
-				.where('store.uuid = :storeUuid and ingredient.uuid = :ingredientUuid', {storeUuid, ingredientUuid: productIng.ingredientUuid})
+				.where('store.uuid = :storeUuid and ingredient.uuid = :ingredientUuid', {
+					storeUuid,
+					ingredientUuid: productIng.ingredientUuid
+				})
 				.getOne() as Ingredient;
 			productIngredient.quantity = productIng.quantity;
 			productIngredient.product = savedProduct;
 			productIngredient.ingredient = ingredient;
 			await this.save(productIngredient);
 		});
-		return
+		return;
 	}
 
-	@Transaction()
-	public async updateStoreProductIngredients(
-		storeUuid: string,
-		productFoundCandidate: Product,
-		product: UpdateProductDto | any,
-		@TransactionManager() productIngredientRepository: Repository<ProductIngredient>) {
-
+	public async updateStoreProductIngredients(storeUuid: string, productFoundCandidate: Product, product: UpdateProductDto | any) {
 		const ingredientRepository = getCustomRepository(IngredientRepository);
 
-		product.ingredients.forEach(async (productIng) => {
-
+		product.ingredients.forEach(async productIng => {
 			const ingredientFound = await ingredientRepository.findStoreIngredientByUuid(storeUuid, productIng.ingredientUuid);
 
-			if(productIng.quantity && ingredientFound){
-
-				const productIngredientFound = await this.getProductIngredient(productFoundCandidate.id,ingredientFound.id);
-
-				if((Object.keys(productIngredientFound).length !== 0)){
-					return await this.updateProductIngredient(productFoundCandidate.id, ingredientFound.id, productIng.quantity);
-				}else{
-
-					let newProductIngredient = new ProductIngredient();
-					newProductIngredient.quantity = productIng.quantity;
-					newProductIngredient.ingredient = ingredientFound;
-					newProductIngredient.product = productFoundCandidate;
-					return this.save(newProductIngredient);
-				}
-
+			if (!(productIng.quantity && ingredientFound)) {
+				return;
 			}
+			const productIngredientFound = await this.getProductIngredient(productFoundCandidate.id, ingredientFound.id);
+			if ((Object.keys(productIngredientFound).length !== 0)) {
+				return this.updateProductIngredient(productFoundCandidate.id, ingredientFound.id, productIng.quantity);
+			}
+
+			const newProductIngredient = new ProductIngredient();
+			newProductIngredient.quantity = productIng.quantity;
+			newProductIngredient.ingredient = ingredientFound;
+			newProductIngredient.product = productFoundCandidate;
+			return this.save(newProductIngredient);
 		});
 		return;
 	}
@@ -85,11 +72,10 @@ export class ProductIngredientRepository extends Repository<ProductIngredient> i
 		return store.store_uuid === storeUuid;
 	}
 
-	public async updateProductIngredient(idProduct: number, idIngredient: number, quantity: number){
-
-		return await createQueryBuilder(ProductIngredient, 'productIngredient')
+	public async updateProductIngredient(idProduct: number, idIngredient: number, quantity: number) {
+		return this.createQueryBuilder('productIngredient')
 			.leftJoinAndSelect('productIngredient.product', 'product')
-			.leftJoinAndSelect('productIngredient.ingredient','ingredient')
+			.leftJoinAndSelect('productIngredient.ingredient', 'ingredient')
 			.where('product.id = :idProduct and ingredient.id = :idIngredient', {idProduct, idIngredient})
 			.update(ProductIngredient)
 			.set({quantity})
@@ -97,11 +83,10 @@ export class ProductIngredientRepository extends Repository<ProductIngredient> i
 
 	}
 
-	public async getProductIngredient(idProduct: number, idIngredient: number){
-
-		return await createQueryBuilder(ProductIngredient, 'productIngredient')
+	public async getProductIngredient(idProduct: number, idIngredient: number) {
+		return this.createQueryBuilder('productIngredient')
 			.leftJoinAndSelect('productIngredient.product', 'product')
-			.leftJoinAndSelect('productIngredient.ingredient','ingredient')
+			.leftJoinAndSelect('productIngredient.ingredient', 'ingredient')
 			.where('product.id = :idProduct and ingredient.id = :idIngredient', {idProduct, idIngredient})
 			.execute();
 	}
