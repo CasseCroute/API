ac5771b4bdc6bff2b185b3e19ed9191209764c3f
ABEDIER <abediernelson@live.fr>
2018-11-16 16:06:55 +0100

    feat: 😉 add product to section repository

--- src/infrastructure/repository/section.repository.ts
+++ src/infrastructure/repository/section.repository.ts
@@ -1,9 +1,12 @@
 import {
-	EntityRepository,
+	EntityRepository, getCustomRepository,
 	Repository,
 } from 'typeorm';
 import {ResourceRepository} from '@letseat/infrastructure/repository/resource.repository';
 import {Section} from '@letseat/domains/section/section.entity';
+import {AddSectionProductDto} from '@letseat/domains/section/dtos/add-section-product.dto';
+import {MealRepository} from '@letseat/infrastructure/repository/meal.repository';
+import {ProductRepository} from '@letseat/infrastructure/repository/product.repository';
 
 @EntityRepository(Section)
 export class SectionRepository extends Repository<Section> implements ResourceRepository {
@@ -30,6 +33,41 @@ export class SectionRepository extends Repository<Section> implements ResourceRe
 			.leftJoinAndSelect('section.products', 'products')
 			.where('section.uuid = :uuid', {uuid: sectionUuid})
 			.getOne();
+	}
+
+	public async addSectionProduct(storeUuid: string, section: AddSectionProductDto) {
+		const mealsRepository = getCustomRepository(MealRepository);
+		const productsRepository = getCustomRepository(ProductRepository);
+		const {sectionUuid, products, meals} = section;
+
+		console.log(sectionUuid + products + meals);
+		const storeSection = await this.findStoreSectionByUuid(storeUuid, sectionUuid) as Section;
+
+		if (!(meals && meals.length > 0)) {
+			return;
+		}
+
+		meals.forEach(mealUuid => {
+			const newMeal = mealsRepository.findOneByUuid(mealUuid);
+
+			if (newMeal) {
+				return;
+			}
+
+			storeSection.meals.push(newMeal);
+			this.save(storeSection);
+
+		});
+
+		products.forEach(productUuid => {
+			const newProduct = productsRepository.findStoreProductByUuid(storeUuid, productUuid);
+			if (newProduct) {
+				return;
+			}
+
+			storeSection.products.push(newProduct);
+			this.save(newProduct);
+		});
 	}
 
 	public async deleteSectionByUuid(storeUuid: string, sectionUuid: string) {
