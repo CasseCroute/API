31a7ec4ebddb938c1ec75c21390f83ac5d0baef5
ABEDIER <abediernelson@live.fr>
2018-11-29 16:01:07 +0100

    feat: ✨ GET vouchers query & handler

--- src/application/queries/voucher/handlers/index.ts
+++ src/application/queries/voucher/handlers/index.ts
@@ -1,12 +1,15 @@
 import {GetVoucherByUuidHandler} from '@letseat/application/queries/voucher/handlers/get-voucher-by-uuid.handler';
 import {GetVoucherByCodeHandler} from './get-voucher-by-code.handler';
+import {GetVouchersHandler} from './get-vouchers.handler';
 
 export const VoucherQueryHandlers = [
 	GetVoucherByUuidHandler,
-	GetVoucherByCodeHandler
+	GetVoucherByCodeHandler,
+	GetVouchersHandler
 ];
 
 export {
 	GetVoucherByUuidHandler,
-	GetVoucherByCodeHandler
+	GetVoucherByCodeHandler,
+	GetVouchersHandler
 };
