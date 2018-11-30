910f4ed30cf7abd90a1501baf49f0c00a19f1551
Alassane Coly-Arogundade <bonjour@alassanecoly.fr>
2018-11-30 23:25:56 +0100

    enhance: add order history

--- src/infrastructure/repository/customer.repository.ts
+++ src/infrastructure/repository/customer.repository.ts
@@ -59,6 +59,8 @@ export class CustomerRepository extends Repository<Customer> {
 		return this.createQueryBuilder('customer')
 			.leftJoinAndSelect('customer.orders', 'orders')
 			.leftJoinAndSelect('orders.store', 'store')
+			.leftJoinAndSelect('orders.history', 'history')
+			.leftJoinAndSelect('history.status', 'historyStatus')
 			.leftJoinAndSelect('orders.detailsMeals', 'orderDetailsMeals')
 			.leftJoinAndSelect('orders.detailsProducts', 'orderDetailsProducts')
 			.leftJoinAndSelect('orderDetailsMeals.meal', 'orderDetailsMeal')
@@ -71,6 +73,7 @@ export class CustomerRepository extends Repository<Customer> {
 			.leftJoinAndSelect('orderDetailsProducts.product', 'orderDetailsProduct')
 			.where('customer.uuid = :customerUuid', {customerUuid})
 			.orderBy('orders.createdAt', 'DESC')
+			.addOrderBy('history.updatedAt', 'DESC')
 			.getOne();
 	}
 }
